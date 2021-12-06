4e2796c3c2603501d746f023219402678ea686fc
Hey OlehPetko! <petkooleg@gmail.com>
2021-12-06 13:44:26 +0000

    new components game

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import TicToeApp from "./games/TicToeApp";
+import {useState} from "react";
+import Game from "./games/Game";
 
 
 function App() {
+    const [visible, setVisible] = useState('')
     return (<div className="App">
-            <h1>GAMES</h1>
-                <TicToeApp/>
-        </div>);
+        <h1>GAMES</h1>
+
+        <button onClick={() => setVisible('TicTakToe')}>TicTakToe</button>
+        <button onClick={() => setVisible('Game')}>Game</button>
+        {visible === 'TicTakToe' && <TicToeApp/>}
+        {visible === 'Game' && <Game />}
+
+
+    </div>);
 }
 
 export default App;
